a021a15aff65f9af2f41b99a7628f12e6c1d9f6d
bangquan.qian <chainz@foxmail.com>
2019-11-08 15:13:54 +0800

    修改代码

---
+++ src/main/java/cn/tenbit/haw/core/extend/support/TransFormFeature.java
@@ -0,0 +1,16 @@
+package cn.tenbit.haw.core.extend.support;
+
+/**
+ * @author bangquan.qian
+ * @date 2019-11-08 13:52
+ */
+public enum TransFormFeature {
+
+    COVER_IF_EXIST,
+
+    IGNORE_IF_EXIST,
+
+    PUT_IF_NULL,
+
+    IGNORE_IF_NULL,
+}
--- src/main/java/cn/tenbit/haw/core/extend/util/HareTransforms.java
+++ src/main/java/cn/tenbit/haw/core/extend/util/HareTransforms.java
@@ -1,9 +1,11 @@
 package cn.tenbit.haw.core.extend.util;
 
+import cn.tenbit.haw.core.extend.support.TransFormFeature;
 import cn.tenbit.haw.core.support.HawExecutor;
 import cn.tenbit.haw.core.util.HawCollections;
 import cn.tenbit.haw.core.util.HawDefaults;
 import cn.tenbit.haw.core.util.HawInvokes;
+import cn.tenbit.haw.core.util.HawObjects;
 
 import java.lang.reflect.Method;
 import java.util.*;
@@ -15,45 +17,60 @@ import java.util.*;
 @SuppressWarnings(HawDefaults.SUPPRESS_WARNING_UNCHECKED)
 public class HareTransforms {
 
-    private static final boolean DEFAULT_COVER_IF_EXIST = false;
+    private static final TransFormFeature[] DEFAULT_FEATURES = {
+            TransFormFeature.IGNORE_IF_EXIST,
+            TransFormFeature.IGNORE_IF_NULL
+    };
 
-    public static <E, S> List<S> list2List(List<E> list, String field, boolean coverIfExist) {
-        Set<S> set = list2Set(list, field, coverIfExist);
+    public static <E, S> List<S> list2List(List<E> list, String field) {
+        return list2List(list, field, DEFAULT_FEATURES);
+    }
+
+    public static <E, S> Set<S> list2Set(List<E> list, String field) {
+        return list2Set(list, field, DEFAULT_FEATURES);
+    }
+
+    public static <K, E> Map<K, E> list2Map(List<E> list, String field) {
+        return list2Map(list, field, DEFAULT_FEATURES);
+    }
+
+    public static <E, S> List<S> list2List(List<E> list, String field, TransFormFeature... features) {
+        Set<S> set = list2Set(list, field, features);
         return HawCollections.isEmpty(set) ? Collections.<S>emptyList() : new ArrayList<S>(set);
     }
 
-    public static <E, S> List<S> list2List(List<E> list, String field) {
-        return list2List(list, field, DEFAULT_COVER_IF_EXIST);
-    }
-
-    public static <E, S> Set<S> list2Set(List<E> list, String field, boolean coverIfExist) {
-        Map<S, E> map = list2Map(list, field, coverIfExist);
+    public static <E, S> Set<S> list2Set(List<E> list, String field, TransFormFeature... features) {
+        Map<S, E> map = list2Map(list, field, features);
         if (HawCollections.isEmpty(map)) {
             return Collections.emptySet();
         }
         return map.keySet();
     }
 
-    public static <E, S> Set<S> list2Set(List<E> list, String field) {
-        return list2Set(list, field, DEFAULT_COVER_IF_EXIST);
-    }
-
-    public static <K, E> Map<K, E> list2Map(List<E> list, String field) {
-        return list2Map(list, field, false);
-    }
-
-    public static <K, E> Map<K, E> list2Map(final List<E> list, final String field, final boolean coverIfExist) {
+    public static <K, E> Map<K, E> list2Map(final List<E> list, final String field, final TransFormFeature... features) {
         return HawInvokes.invokeWithTurnRe(new HawExecutor<Map<K, E>>() {
             @Override
             public Map<K, E> execute() throws Throwable {
-                return doList2Map(list, field, coverIfExist);
+                return doList2Map(list, field, features);
             }
         });
     }
 
-    private static <K, E> Map<K, E> doList2Map(List<E> list, String field, boolean coverIfExist) throws Exception {
+    private static <K, E> Map<K, E> doList2Map(List<E> list, String field, TransFormFeature... features) throws Exception {
         if (HawCollections.isEmpty(list)) {
             return Collections.emptyMap();
+        }
+
+        List<TransFormFeature> featureList = HawObjects.emptyIfNull(HawCollections.asList(features));
+
+        boolean coverIfExist = true;
+        if (featureList.contains(TransFormFeature.IGNORE_IF_EXIST)) {
+            coverIfExist = false;
+        }
+
+        boolean ignoreIfNull = true;
+        if (featureList.contains(TransFormFeature.PUT_IF_NULL)) {
+            ignoreIfNull = false;
         }
 
         Method rm = HareBeans.getGetterMethod(list.get(0).getClass(), field);
@@ -63,9 +80,13 @@ public class HareTransforms {
         for (E e : list) {
             K key = (K) rm.invoke(e);
             E val = map.get(key);
-            if (coverIfExist || val == null) {
-                map.put(key, e);
+            if (key == null && ignoreIfNull) {
+                continue;
             }
+            if (val != null && !coverIfExist) {
+                continue;
+            }
+            map.put(key, e);
         }
 
         return map;
